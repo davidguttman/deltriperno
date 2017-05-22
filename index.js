5f388c109397cc4ce010d687a9d1a4c8c3142adc
David Guttman <david@davidguttman.com>
2017-05-22 09:34:29 -0700

    cleanup / refactor

--- index.js
+++ index.js
@@ -4,128 +4,83 @@ var getDistance = require('euclidean-distance')
 
 var simplex = new Simplex()
 
-var SPEED = 2.75
-var N_POINTS = 120
-
-var width = 0.75 * window.innerWidth
-var height = 0.75 * window.innerHeight
+var SPEED = 0.001
+var N_POINTS = 200
 
 document.body.style.background = 'rgb(20, 20, 20)'
 
-var points = createPoints(N_POINTS, width, height)
+var stage = createStage()
+document.body.appendChild(stage)
 
-points.forEach(function (point) {
-  // document.body.appendChild(point.el)
-})
-
-var parent = document.createElement('div')
-parent.style.position = 'absolute'
-parent.style.top = 0.125 * window.innerHeight + 'px'
-parent.style.left = 0.125 * window.innerWidth + 'px'
-document.body.appendChild(parent)
+var points = createPoints(N_POINTS)
 var triangles = createTrianges(points)
-parent.appendChild(triangles.el)
+stage.appendChild(triangles.el)
 
 window.requestAnimationFrame(updateLoop)
 
-var fps = 0
-setInterval(function () {
-  console.log('fps', fps)
-  fps = 0
-}, 1000)
 function updateLoop () {
-  fps += 1
   window.requestAnimationFrame(updateLoop)
-  points.forEach(function (point, i) {
-    // var theta = (0.5 - Math.random()) * (Math.PI / 16)
-    var x = point.x / 40
-    var y = point.y / 40
-    var z = Date.now() / 10000
-    var s = simplex.noise3d(x, y, z)
-    // var s = simplex.noise(x, y)
-    var theta1 = s * (2 * Math.PI)
-    var theta = (0.01 * theta1) + (0.99 * point.theta)
-    // if (i === 0) console.log(s, theta)
-    point.move(theta, SPEED)
-  })
+  points.forEach(function (point) { point.update() })
   triangles.update(points)
 }
 
-function createPoints (n, w, h) {
+function createPoints (n) {
   var points = []
 
-  var x, y
   for (var i = 0; i < n; i++) {
-    x = Math.floor(Math.random() * w)
-    y = Math.floor(Math.random() * h)
-    points.push(createPoint([x, y]))
+    points.push({
+      x: Math.random(),
+      y: Math.random(),
+      theta: Math.random() * 2 * Math.PI,
+      update: updatePoint
+    })
   }
 
   return points
 }
 
-function createPoint (coords) {
-  var w = 20
-  var h = w
+function updatePoint () {
+  var nx = this.x * 10
+  var ny = this.y * 10
+  var nz = Date.now() / 10000
+  var s = simplex.noise3d(nx, ny, nz)
 
-  var el = document.createElement('div')
-  el.style.position = 'absolute'
-  el.style.left = coords[0] + 'px'
-  el.style.top = coords[1] + 'px'
-  el.style.width = w + 'px'
-  el.style.height = h + 'px'
-  el.style.borderRadius = w / 2 + 'px'
-  el.style.background = '#aaa'
+  var theta = s * 2 * Math.PI
+  this.theta = (0.01 * theta) + (0.99 * this.theta)
 
-  var point = {
-    el: el,
-    x: coords[0],
-    y: coords[1],
-    theta: Math.random() * 2 * Math.PI,
-    move: move
-  }
+  this.x += SPEED * Math.cos(this.theta)
+  this.y += SPEED * Math.sin(this.theta)
 
-  return point
-}
-
-function mod (a, n) {
-  return ((a % n) + n) % n
-}
-
-function move (theta, r) {
-  this.theta = theta
-  this.x += r * Math.cos(this.theta)
-  this.y += r * Math.sin(this.theta)
-
-  this.x = mod(this.x, width)
-  this.y = mod(this.y, height)
-
-  this.el.style.left = this.x + 'px'
-  this.el.style.top = this.y + 'px'
+  this.x = mod(this.x, 1)
+  this.y = mod(this.y, 1)
 }
 
 function createTrianges (points) {
-  var stroke = '#ccc'
-  var fill = 'rgba(0,0,0,0)'
-  var viewBox = [0, 0, width, height].join(' ')
+  var viewBox = [0, 0, 1, 1].join(' ')
 
   var pathData = createPathData(points)
 
   var parent = document.createElement('div')
+  parent.style.width = '100%'
+  parent.style.height = '100%'
   parent.innerHTML = `
     <svg xmlns='http://www.w3.org/svg/2000'
       viewBox='${viewBox}'
-      width=${width}
-      height=${height}
+      width='100%'
+      height='100%'
       stroke='none'
       fill='none'>
       ${pathData}
     </svg>
   `
+
+  var svg = parent.children[0]
+
   return {
     el: parent,
-    svg: parent.querySelector('svg'),
-    update: update
+    update: function (points) {
+      svg.innerHTML = createPathData(points)
+    }
   }
 }
 
@@ -137,43 +92,37 @@ function createPathData (points) {
   var triangles = new Delaunator(coords).triangles
 
   var pathData = []
-
-  var maxArea = 2 * (height * width) / (N_POINTS / 3)
-
-  var x0, y0, x1, y1, x2, y2, d, fill, b, eq
   for (var i = 0; i < triangles.length; i += 3) {
-    x0 = coords[triangles[i]][0]
-    y0 = coords[triangles[i]][1]
-    x1 = coords[triangles[i + 1]][0]
-    y1 = coords[triangles[i + 1]][1]
-    x2 = coords[triangles[i + 2]][0]
-    y2 = coords[triangles[i + 2]][1]
-
-    d = [
-      'M', x0, y0,
-      'L', x1, y1,
-      'L', x2, y2,
-      'L', x0, y0
-    ].join(' ')
-
-    // area = calcArea(x0, y0, x1, y1, x2, y2)
-    eq = equalness(x0, y0, x1, y1, x2, y2)
-
-    // b = Math.floor((area / maxArea) * 255)
-    b = Math.floor(20 + (eq * 150))
-    fill = `rgb(${b}, ${b}, ${b})`
-    pathData.push(`<path d='${d}' fill='${fill}' stroke='${fill}' />`)
+    pathData.push(createTrianglePath(
+      coords[triangles[i + 0]],
+      coords[triangles[i + 1]],
+      coords[triangles[i + 2]]
+    ))
   }
 
   return pathData.join('\n')
 }
 
-function update (points) {
-  this.svg.innerHTML = createPathData(points)
-}
+function createTrianglePath (pa, pb, pc) {
+  var x0 = pa[0]
+  var y0 = pa[1]
+  var x1 = pb[0]
+  var y1 = pb[1]
+  var x2 = pc[0]
+  var y2 = pc[1]
 
-function calcArea (x0, y0, x1, y1, x2, y2) {
-  return Math.abs(((x0 * (y1 - y2)) + (x1 * (y2 - y0)) + (x2 * (y0 - y1))) / 2)
+  var d = [
+    'M', x0, y0,
+    'L', x1, y1,
+    'L', x2, y2,
+    'L', x0, y0
+  ].join(' ')
+
+  var eq = equalness(x0, y0, x1, y1, x2, y2)
+
+  var b = Math.floor(20 + (eq * 150))
+  var fill = `rgb(${b}, ${b}, ${b})`
+  return `<path d='${d}' fill='${fill}' stroke='none' />`
 }
 
 function equalness (x0, y0, x1, y1, x2, y2) {
@@ -182,4 +131,18 @@ function equalness (x0, y0, x1, y1, x2, y2) {
   var c = getDistance([x2, y2], [x1, y1])
   var diff = Math.abs(a - b) + Math.abs(a - c) + Math.abs(b - c)
   return 1 - (diff / (a + b + c))
+}
+
+function createStage (w, h) {
+  var stage = document.createElement('div')
+  stage.style.position = 'absolute'
+  stage.style.top = '10%'
+  stage.style.left = '10%'
+  stage.style.width = '80%'
+  stage.style.height = '80%'
+  return stage
+}
+
+function mod (a, n) {
+  return ((a % n) + n) % n
 }
